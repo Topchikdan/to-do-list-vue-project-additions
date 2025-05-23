852a7e8add0bf16d9e9ff0a84635cdf6c911a3a5
Daniil Kuznetsov <platabush@mail.com>
2025-05-23 20:41:10 +0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
 
+import router from './router'
+
 const vuetify = createVuetify({
   components,
   directives,
@@ -15,4 +17,4 @@ const vuetify = createVuetify({
   },
 })
 
-createApp(App).use(vuetify).mount('#app')
+createApp(App).use(vuetify).use(router).mount('#app')
